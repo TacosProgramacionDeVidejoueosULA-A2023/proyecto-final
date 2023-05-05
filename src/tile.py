9ee013a9d6ecbb1a56b43f123463d621801274e0
Jorge Luis Rodríguez González <egroj97@gmail.com>
2023-05-05 18:07:20 -0400

    Created first draft for final project

---
+++ src/tile.py
@@ -0,0 +1,12 @@
+import pygame
+from .settings import *
+import os
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(
+            os.getcwd() + "/assets/graphics/test/rock.png"
+        ).convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
